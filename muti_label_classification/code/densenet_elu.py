05ba82ec7fd6f8a1e51cf0135f6630a0c4f3b304
Developer <rubans@Rubans.local>
2018-04-14 21:29:17 +0800

    daliy updata

--- muti_label_classification/code/densenet_elu.py
+++ muti_label_classification/code/densenet_elu.py
@@ -149,8 +149,8 @@ def densenet(inputs,
       #                   biases_initializer=tf.zeros_initializer(),
       #                   scope='logits')
       net = slim.flatten(net)
-      if fc_dropout_rate:
-        net = slim.dropout(net, fc_dropout_rate, is_training=is_training, scope='Dropout')
+      # if fc_dropout_rate:
+      #   net = slim.dropout(net, fc_dropout_rate, is_training=is_training, scope='Dropout')
       logits = slim.fully_connected(net, num_classes, activation_fn=None,
                                     scope='Logits')
 
