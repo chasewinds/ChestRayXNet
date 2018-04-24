15e36dfd649966bf3abb5284726cefb23d1b76c2
Developer <rubans@Rubans.local>
2018-04-24 19:41:41 +0800

    weight loss update

--- ChestRayXNet/shell/train_pneumonia.sh
+++ ChestRayXNet/shell/train_pneumonia.sh
@@ -6,11 +6,11 @@ python code/train_densenet.py \
     --tfrecord_dir 'data/tfrecord/pneumonia' \
     --tfrecord_prefix 'pneumonia' \
     --log_dir 'log/pne_log' \
-    --log_txt_path 'log/pneumonia/validation_log.txt' \
+    --log_txt_path 'log/pne_log/validation_log.txt' \
     --image_label_list 'data/list/pneumonia/image_label.txt' \
     --model_type 'densenet121' \
     --checkpoint_file 'model/dense121/tf-densenet121.ckpt' \
-     --num_classes 1 \
+    --num_classes 1 \
     --num_epoch 100 \
     --batch_size 16 \
     --step_size 15 \
