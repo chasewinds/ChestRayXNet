95adbb6838a6f02435ec00e9ee0a37b8ebef1f59
Developer <rubans@Rubans.local>
2018-04-28 20:59:02 +0800

    train_clean

--- ChestRayXNet/code/data_preproces.py
+++ ChestRayXNet/code/data_preproces.py
@@ -1,4 +1,3 @@
-"""Provides utilities to preprocess images for networks."""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -6,25 +5,6 @@ from __future__ import print_function
 import tensorflow as tf
 
 from tensorflow.python.ops import control_flow_ops
-
-
-def apply_with_random_selector(x, func, num_cases):
-  """Computes func(x, sel), with sel sampled from [0...num_cases-1].
-
-  Args:
-    x: input Tensor.
-    func: Python function to apply.
-    num_cases: Python int32, number of cases to sample sel from.
-
-  Returns:
-    The result of func(x, sel), where func receives the value of the
-    selector as a python integer, but sel is sampled dynamically.
-  """
-  sel = tf.random_uniform([], maxval=num_cases, dtype=tf.int32)
-  # Pass the real x only to one of the func calls.
-  return control_flow_ops.merge([
-      func(control_flow_ops.switch(x, tf.equal(sel, case))[1], case)
-      for case in range(num_cases)])[0]
 
 def preprocess_for_train(image, height, width, scope=None):
   with tf.name_scope(scope, 'distort_image', [image, height, width]):
@@ -42,7 +22,7 @@ def preprocess_for_train(image, height, width, scope=None):
     distorted_image = tf.image.random_flip_left_right(image)
 
     base_color_scale = tf.constant([255.0])
-    image = tf.subtract(image, base_color_scale)
+    image = tf.subtract(distorted_image, base_color_scale)
     # the mean and std of ImageNet is as fellow:
     total_mean = tf.constant([0.485, 0.456, 0.406])
     std = tf.constant([0.229, 0.224, 0.225])
@@ -81,25 +61,7 @@ def preprocess_image(image, height, width,
                      is_training=False,
                      bbox=None,
                      fast_mode=True):
-  """Pre-process one image for training or evaluation.
 
-  Args:
-    image: 3-D Tensor [height, width, channels] with the image.
-    height: integer, image expected height.
-    width: integer, image expected width.
-    is_training: Boolean. If true it would transform an image for train,
-      otherwise it would transform it for evaluation.
-    bbox: 3-D float Tensor of bounding boxes arranged [1, num_boxes, coords]
-      where each coordinate is [0, 1) and the coordinates are arranged as
-      [ymin, xmin, ymax, xmax].
-    fast_mode: Optional boolean, if True avoids slower transformations.
-
-  Returns:
-    3-D float Tensor containing an appropriately scaled image
-
-  Raises:
-    ValueError: if user does not provide bounding box
-  """
   # image = tf.image.resize_image_with_crop_or_pad(image, height, width)
   if is_training:
     return preprocess_for_train(image, height, width)
