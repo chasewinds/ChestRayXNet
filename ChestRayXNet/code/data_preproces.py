21cce4750994e0d717e03f0d18ec5ab4e25c93c7
Developer <rubans@Rubans.local>
2018-04-28 20:56:19 +0800

    train_clean

--- ChestRayXNet/code/data_preproces.py
+++ ChestRayXNet/code/data_preproces.py
@@ -27,31 +27,6 @@ def apply_with_random_selector(x, func, num_cases):
       for case in range(num_cases)])[0]
 
 def preprocess_for_train(image, height, width, scope=None):
-  """Distort one image for training a network.
-
-  Distorting images provides a useful technique for augmenting the data
-  set during training in order to make the network invariant to aspects
-  of the image that do not effect the label.
-
-  Additionally it would create image_summaries to display the different
-  transformations applied to the image.
-
-  Args:
-    image: 3-D Tensor of image. If dtype is tf.float32 then the range should be
-      [0, 1], otherwise it would converted to tf.float32 assuming that the range
-      is [0, MAX], where MAX is largest positive representable number for
-      int(8/16/32) data type (see `tf.image.convert_image_dtype` for details).
-    height: integer
-    width: integer
-    bbox: 3-D float Tensor of bounding boxes arranged [1, num_boxes, coords]
-      where each coordinate is [0, 1) and the coordinates are arranged
-      as [ymin, xmin, ymax, xmax].
-    fast_mode: Optional boolean, if True avoids slower transformations (i.e.
-      bi-cubic resizing, random_hue or random_contrast).
-    scope: Optional scope for name_scope.
-  Returns:
-    3-D float Tensor of distorted image used for training with range [-1, 1].
-  """
   with tf.name_scope(scope, 'distort_image', [image, height, width]):
     if image.dtype != tf.float32:
       image = tf.image.convert_image_dtype(image, dtype=tf.float32)
@@ -79,32 +54,12 @@ def preprocess_for_train(image, height, width, scope=None):
     return image
 
 
-def preprocess_for_eval(image, height, width,
-                        central_fraction=0.875, scope=None):
-  """Prepare one image for evaluation.
-
-  If height and width are specified it would output an image with that size by
-  applying resize_bilinear.
-
-  If central_fraction is specified it would cropt the central fraction of the
-  input image.
-
-  Args:
-    image: 3-D Tensor of image. If dtype is tf.float32 then the range should be
-      [0, 1], otherwise it would converted to tf.float32 assuming that the range
-      is [0, MAX], where MAX is largest positive representable number for
-      int(8/16/32) data type (see `tf.image.convert_image_dtype` for details)
-    height: integer
-    width: integer
-    central_fraction: Optional Float, fraction of the image to crop.
-    scope: Optional scope for name_scope.
-  Returns:
-    3-D float Tensor of prepared image.
-  """
+def preprocess_for_eval(image, height, width, central_fraction=0.875, scope=None):
   with tf.name_scope(scope, 'eval_image', [image, height, width]):
     if image.dtype != tf.float32:
       image = tf.image.convert_image_dtype(image, dtype=tf.float32)
 
+    image.set_shape([None, None, 3])
     if height and width:
       # Resize the image to the specified height and width.
       image = tf.expand_dims(image, 0)
