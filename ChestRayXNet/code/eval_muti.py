cf70463e1b9009259148a1bded02c90b3e5d282b
Developer <rubans@Rubans.local>
2018-04-28 21:05:09 +0800

    train_clean

--- ChestRayXNet/code/eval_muti.py
+++ ChestRayXNet/code/eval_muti.py
@@ -135,7 +135,7 @@ def run():
 
         #Get your supervisor
 
-        sv = tf.train.Supervisor(logdir=FLAGS.log_eval, summary_op=None, saver-None, init_fn=restore_fn)
+        sv = tf.train.Supervisor(logdir=FLAGS.log_eval, summary_op=None, saver=None, init_fn=restore_fn)
         #Now we are ready to run in one session
         with sv.managed_session() as sess:
             for step in xrange(num_batches_per_epoch * FLAGS.num_epochs):
