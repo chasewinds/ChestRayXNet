3e4e68a4b347df80bcf0e3e6b5ef600db9d5fee3
Developer <rubans@Rubans.local>
2018-04-28 20:48:39 +0800

    train_clean

--- ChestRayXNet/code/eval_muti.py
+++ ChestRayXNet/code/eval_muti.py
@@ -5,6 +5,7 @@ from sklearn.metrics import roc_auc_score
 
 import tensorflow as tf
 from tensorflow.python.platform import tf_logging as logging
+from tensorflow.python.ops import math_ops
 from tensorflow.contrib.framework.python.ops.variables import get_or_create_global_step
 # from inception_resnet_v2 import inception_resnet_v2, inception_resnet_v2_arg_scope
 from densenet import densenet121, densenet_arg_scope
