53bc7e96856ceebe6f398b5a03656804d74a58e7
Developer <rubans@Rubans.local>
2018-05-07 13:17:28 +0800

    train_clean

--- ChestRayXNet/code/train_clean.py
+++ ChestRayXNet/code/train_clean.py
@@ -169,21 +169,7 @@ def run():
         # total_loss = tf.losses.sigmoid_cross_entropy(multi_class_labels=train_labels, logits=logits)
         # logging.info("The shape of loss is : %s" % total_loss.get_shape())
         def focal_loss(labels, logits, gamma=2.0, alpha=4.0):
-            """
-            focal loss for multi-classification
-            FL(p_t)=-alpha(1-p_t)^{gamma}ln(p_t)
-            Notice: logits is probability after softmax
-            gradient is d(Fl)/d(p_t) not d(Fl)/d(x) as described in paper
-            d(Fl)/d(p_t) * [p_t(1-p_t)] = d(Fl)/d(x)
-            Lin, T.-Y., Goyal, P., Girshick, R., He, K., & Dollár, P. (2017).
-            Focal Loss for Dense Object Detection, 130(4), 485–491.
-            https://doi.org/10.1016/j.ajodo.2005.02.022
-            :param labels: ground truth labels, shape of [batch_size]
-            :param logits: model's output, shape of [batch_size, num_cls]
-            :param gamma:
-            :param alpha:
-            :return: shape of [batch_size]
-            """
+            
             epsilon = 1.e-9
             labels = tf.to_int64(labels)
             labels = tf.convert_to_tensor(labels, tf.int64)
