f7bff75f5e04d96f4d7f861ecebcafc4791c7b49
Developer <rubans@Rubans.local>
2018-05-19 09:31:29 +0800

    loss modify

--- ChestRayXNet/code/train_pneumonia.py
+++ ChestRayXNet/code/train_pneumonia.py
@@ -139,10 +139,12 @@ def run():
             # weight:0: 0.012736654326434312, 1: 0.9872633456735657
             epsilon = 1e-8
             
-            return -0.987*math_ops.multiply(labels, math_ops.log(predictions + epsilon)) - 0.012*math_ops.multiply(
+            loss =  -0.987*math_ops.multiply(labels, math_ops.log(predictions + epsilon)) - 0.012*math_ops.multiply(
             (1 - labels), math_ops.log(1 - predictions + epsilon))
-        binary_crossentropy = weighted_cross_entropy(logits, train_labels)
-        total_loss = tf.reduce_sum(binary_crossentropy)
+            return tf.reduce_sum(loss)
+        loss = weighted_cross_entropy(logits, train_labels)
+        total_loss = tf.losses.get_total_loss()
+
 
         ## convert into actual predicte
         lesion_pred = tf.cast(tf.greater_equal(probabilities, 0.5), tf.float32)
